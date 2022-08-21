10161c9fb6282e294ec3e2b75e312fd91d995182
Anthony Quizon <accounts@anthonyquizon.io>
2022-08-21 19:22:00 +1000

    colour

--- dminus/colour.d
+++ dminus/colour.d
@@ -1,3 +1,10 @@
 module dminus.colour;
 
+import prelude;
 
+struct RGBA { ひ r=0, g=0, b=0, a=1.0; }
+
+RGBA red(ひ a=1)   => RGBA(255, 0, 0, a); 
+RGBA green(ひ a=1) => RGBA(0, 255, 0, a); 
+RGBA blue(ひ a=1)  => RGBA(0, 0, 255, a); 
+RGBA black(ひ a=1) => RGBA(0, 0, 0, a); 
