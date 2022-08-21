baee5f24f4ef6dc043283ec4165617b2e203e989
Anthony Quizon <accounts@anthonyquizon.io>
2022-08-21 12:43:56 +1000

    init prelude

---
+++ src/prelude.d
@@ -0,0 +1,13 @@
+module dminus.prelude;
+
+import core.stdc.stdlib : atoi;
+import core.stdc.stdio : sprintf;
+import core.bitop;
+import core.stdc.string : strlen, memcmp;
+import core.stdc.stddef : wchar_t;
+import C=core.stdc.stdio;
+import core.stdc.stdlib, core.sys.posix.unistd;
+
+public import core.stdc.stdio : FILE;
+
+alias つ = string, し = size_t, ひ=double, ろ=bool, ʘ=void, の=char, ㄅ=ubyte, ㄋㄙ=uint, ㄙ=int, 〆=ulong, て=float;
